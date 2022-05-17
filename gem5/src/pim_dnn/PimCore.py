4658ebf6e113bb51ca0356930498bff30d00b9db
qilinzheng <qz91@duke.edu>
2022-05-16 22:00:29 -0400

    initial commit for pim_dnn accelerator

---
+++ gem5/src/pim_dnn/PimCore.py
@@ -0,0 +1,11 @@
+from httplib2 import Response
+from m5.params import *
+from m5.objects.ClockedObject import ClockedObject
+from m5.proxy import *
+
+class PimCore(ClockedObject):
+    type = 'PimCore'
+    cxx_header = "pim_dnn/pim_core.hh"
+    cxx_class = 'gem5::PimCore'
+    dataPort = RequestPort("Memory side port, sends requests")
+    interuptPort = ResponsePort("Controller side port, sends requests")    
